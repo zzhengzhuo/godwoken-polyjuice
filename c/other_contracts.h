fd066848c5783db56beebba1639447b7ce6893fd
zz <517669936@qq.com>
2022-03-10 11:19:27 +0800

    add new precompiled contract

--- c/other_contracts.h
+++ c/other_contracts.h
@@ -9,9 +9,10 @@
 #define RECOVER_ACCOUNT_GAS 3600 /* more than ecrecover */
 #define ETH_TO_GODWOKEN_ADDR_GAS 300
 
-int recover_account_gas(const uint8_t* input_src,
+int recover_account_gas(const uint8_t *input_src,
                         const size_t input_size,
-                        uint64_t* gas) {
+                        uint64_t *gas)
+{
   *gas = RECOVER_ACCOUNT_GAS;
   return 0;
 }
@@ -30,14 +31,16 @@ int recover_account_gas(const uint8_t* input_src,
   =======
     output[0..32] => account script hash
  */
-int recover_account(gw_context_t* ctx,
-                    const uint8_t* code_data,
+int recover_account(gw_context_t *ctx,
+                    const uint8_t *code_data,
                     const size_t code_size,
                     bool is_static_call,
-                    const uint8_t* input_src,
+                    const uint8_t *input_src,
                     const size_t input_size,
-                    uint8_t** output, size_t* output_size) {
-  if (input_size < 128) {
+                    uint8_t **output, size_t *output_size)
+{
+  if (input_size < 128)
+  {
     debug_print_int("input size too small", input_size);
     return ERROR_RECOVER_ACCOUNT;
   }
@@ -47,28 +50,35 @@ int recover_account(gw_context_t* ctx,
   uint8_t *signature = (uint8_t *)input_src + 128;
   uint64_t signature_len = 0;
   ret = parse_u64(input_src + 96, &signature_len);
-  if (ret != 0) {
+  if (ret != 0)
+  {
     debug_print_int("parse signature length failed", ret);
     return ERROR_RECOVER_ACCOUNT;
   }
-  if (signature_len + 128 > input_size) {
+  if (signature_len + 128 > input_size)
+  {
     debug_print_int("invalid input_size", input_size);
     return ERROR_RECOVER_ACCOUNT;
   }
   uint8_t script[GW_MAX_SCRIPT_SIZE];
   uint64_t script_len = GW_MAX_SCRIPT_SIZE;
   ret = ctx->sys_recover_account(ctx, message, signature, signature_len, code_hash, script, &script_len);
-  if (ret != 0) {
+  if (ret != 0)
+  {
     debug_print_int("call sys_recover_account failed", ret);
     /* wrong code_hash is fatal, so we return the error code here */
-    if (is_fatal_error(ret)) {
+    if (is_fatal_error(ret))
+    {
       return FATAL_PRECOMPILED_CONTRACTS;
-    } else {
+    }
+    else
+    {
       return ERROR_RECOVER_ACCOUNT;
     }
   }
   *output = (uint8_t *)malloc(32);
-  if (*output == NULL) {
+  if (*output == NULL)
+  {
     ckb_debug("malloc failed");
     return FATAL_PRECOMPILED_CONTRACTS;
   }
@@ -77,9 +87,10 @@ int recover_account(gw_context_t* ctx,
   return 0;
 }
 
-int eth_to_godwoken_addr_gas(const uint8_t* input_src,
-                           const size_t input_size,
-                           uint64_t* gas) {
+int eth_to_godwoken_addr_gas(const uint8_t *input_src,
+                             const size_t input_size,
+                             uint64_t *gas)
+{
   *gas = ETH_TO_GODWOKEN_ADDR_GAS;
   return 0;
 }
@@ -89,23 +100,28 @@ int eth_to_godwoken_addr_gas(const uint8_t* input_src,
  input:
  ======
    input[12..32] => ETH address
+   input[32..64] => eth lock script hash
 
  output:
    output[12..32] => godwoken short address
  */
-int eth_to_godwoken_addr(gw_context_t* ctx,
-                         const uint8_t* code_data,
+int eth_to_godwoken_addr_other(gw_context_t *ctx,
+                         const uint8_t *code_data,
                          const size_t code_size,
                          bool is_static_call,
-                         const uint8_t* input_src,
+                         const uint8_t *input_src,
                          const size_t input_size,
-                         uint8_t** output, size_t* output_size) {
-  if (input_size < 32) {
+                         uint8_t **output, size_t *output_size)
+{
+  if (input_size < 64)
+  {
     debug_print_int("input size too small", input_size);
     return ERROR_ETH_TO_GODWOKEN_ADDR;
   }
-  for (int i = 0; i < 12; i++) {
-    if (input_src[i] != 0) {
+  for (int i = 0; i < 12; i++)
+  {
+    if (input_src[i] != 0)
+    {
       ckb_debug("invalid ETH address");
       return ERROR_ETH_TO_GODWOKEN_ADDR;
     }
@@ -113,12 +129,15 @@ int eth_to_godwoken_addr(gw_context_t* ctx,
   int ret;
   uint8_t script_args[SCRIPT_ARGS_LEN];
   memcpy(script_args, g_rollup_script_hash, 32);
-  memcpy(script_args + 32, (uint8_t*)(&g_creator_account_id), 4);
+  memcpy(script_args + 32, (uint8_t *)(&g_creator_account_id), 4);
   memcpy(script_args + 32 + 4, input_src + 12, 20);
+  uint8_t lock_script_hash[32];
+  memcpy(lock_script_hash, input_src + 32, 32);
   mol_seg_t new_script_seg;
-  ret = build_script(g_script_code_hash, g_script_hash_type, script_args,
+  ret = build_script(lock_script_hash, 1, script_args,
                      SCRIPT_ARGS_LEN, &new_script_seg);
-  if (ret != 0) {
+  if (ret != 0)
+  {
     return ret;
   }
   uint8_t script_hash[32];
@@ -126,7 +145,8 @@ int eth_to_godwoken_addr(gw_context_t* ctx,
   free(new_script_seg.ptr);
 
   *output = (uint8_t *)malloc(32);
-  if (*output == NULL) {
+  if (*output == NULL)
+  {
     ckb_debug("malloc failed");
     return FATAL_PRECOMPILED_CONTRACTS;
   }
@@ -144,19 +164,23 @@ int eth_to_godwoken_addr(gw_context_t* ctx,
  output:
    output[12..32] => godwoken short address
  */
-int eth_to_godwoken_addr_other(gw_context_t* ctx,
-                         const uint8_t* code_data,
-                         const size_t code_size,
-                         bool is_static_call,
-                         const uint8_t* input_src,
-                         const size_t input_size,
-                         uint8_t** output, size_t* output_size) {
-  if (input_size < 32) {
+int eth_to_godwoken_addr(gw_context_t *ctx,
+                               const uint8_t *code_data,
+                               const size_t code_size,
+                               bool is_static_call,
+                               const uint8_t *input_src,
+                               const size_t input_size,
+                               uint8_t **output, size_t *output_size)
+{
+  if (input_size < 32)
+  {
     debug_print_int("input size too small", input_size);
     return ERROR_ETH_TO_GODWOKEN_ADDR;
   }
-  for (int i = 0; i < 12; i++) {
-    if (input_src[i] != 0) {
+  for (int i = 0; i < 12; i++)
+  {
+    if (input_src[i] != 0)
+    {
       ckb_debug("invalid ETH address");
       return ERROR_ETH_TO_GODWOKEN_ADDR;
     }
@@ -170,15 +194,18 @@ int eth_to_godwoken_addr_other(gw_context_t* ctx,
   mol_seg_t new_script_seg;
   ret = build_script(g_script_code_hash, g_script_hash_type, script_args,
                      52, &new_script_seg);
-  if (ret != 0) {
+  if (ret != 0)
+  {
     return ret;
   }
   uint8_t script_hash[32];
   blake2b_hash(script_hash, new_script_seg.ptr, new_script_seg.size);
   free(new_script_seg.ptr);
 
+  debug_print_data("script seg: ", new_script_seg.ptr, new_script_seg.size);
   *output = (uint8_t *)malloc(32);
-  if (*output == NULL) {
+  if (*output == NULL)
+  {
     ckb_debug("malloc failed");
     return FATAL_PRECOMPILED_CONTRACTS;
   }
@@ -187,4 +214,4 @@ int eth_to_godwoken_addr_other(gw_context_t* ctx,
   return 0;
 }
 
-#endif  /* #define OTHER_CONTRACTS_H_ */
+#endif /* #define OTHER_CONTRACTS_H_ */
